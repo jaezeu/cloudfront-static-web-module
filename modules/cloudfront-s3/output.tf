8e37aa5c11d90c1879f5ee76708620dce1ed71f0
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-09-16 09:03:59 +0800

    add output

--- modules/cloudfront-s3/output.tf
+++ modules/cloudfront-s3/output.tf
@@ -2,6 +2,10 @@ output "cloudfront_domain" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
 
+output "cloudfront_id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
+
 output "bucket_name" {
   value = aws_s3_bucket.static_web.id
 }
--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "domain" {
   value = module.static_web_stack.cloudfront_domain
 }
 
+output "id" {
+  value = module.static_web_stack.cloudfront_id
+}
+
 output "bucket_name" {
   value = module.static_web_stack.bucket_name
 }
